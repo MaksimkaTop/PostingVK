6d807aeffd02ffd15d787adc3c5db4291ac88b20
Maksim Topychkanov <maksim.topychkanov@motorsport.com>
2018-05-30 16:58:30 +0500

    add getGroup for User

--- app/src/main/java/com/example/maks/postingvk/MainActivity.kt
+++ app/src/main/java/com/example/maks/postingvk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.maks.postingvk
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun initVk() {
+    private fun initVk() {
         login_btn.setOnClickListener {
             VKSdk.initialize(applicationContext)
             VKSdk.login(this, *scope)
@@ -40,8 +41,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, object : VKCallback<VKAccessToken> {
                     override fun onResult(res: VKAccessToken) {
-                        // Toast.makeText(applicationContext, res.accessToken, Toast.LENGTH_LONG).show()
-                        VkRequest()
+                        getDataFromGroup()
                     }
 
                     override fun onError(error: VKError) {
@@ -55,6 +55,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun VkRequest() {
         val request = VKApi.users().get(VKParameters.from(VKApiConst.FIELDS, VKApiUser.FIELD_PHOTO_200))
+
+        Log.wtf("qwe", request.toString())
         request.executeWithListener(object : VKRequest.VKRequestListener() {
             override fun onComplete(response: VKResponse?) {
                 val user = (response!!.parsedModel as VKList<VKApiUser>)[0]
@@ -64,6 +66,22 @@ class MainActivity : AppCompatActivity() {
                         .load(urlImage)
                         .apply(RequestOptions.circleCropTransform())
                         .into(icon)
+            }
+
+            override fun onError(error: VKError?) {}
+            override fun attemptFailed(request: VKRequest?, attemptNumber: Int, totalAttempts: Int) {}
+        })
+    }
+
+    private fun getDataFromGroup(){
+       // val requestGroup = VKApi.groups().get(VKParameters.from(VKApiConst.USER_ID,"-20629724", VKApiConst.POST_ID, "1068924", VKApiConst.COUNT, 10 ))
+        val requestGroup = VKApi.groups().get(VKParameters.from(VKApiConst.EXTENDED,1,VKApiConst.COUNT, 10))
+        Log.wtf("qwe",requestGroup.toString())
+        requestGroup.executeWithListener(object : VKRequest.VKRequestListener() {
+            override fun onComplete(response: VKResponse?) {
+              val rr =   response?.json
+                Log.wtf("qwe", "rr response" + response?.json)
+
             }
 
             override fun onError(error: VKError?) {}
